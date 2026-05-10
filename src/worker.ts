526c65318646819dab20ec11efbaecad740e5fb7
hakhawaj <hakhawaj@uwaterloo.ca>
2026-05-10 00:49:17 -0400

    Track worker jobs processed

--- src/worker.ts
+++ src/worker.ts
@@ -14,6 +14,7 @@ import {
     registerWorker,
     heartbeatWorker,
     markWorkerStopped,
+    incrementWorkerJobsProcessed
 } from "./workers.js";
 
 const hostname = os.hostname();
@@ -108,6 +109,8 @@ async function workerLoop() {
             });
 
             await completeJob(job.id);
+
+            await incrementWorkerJobsProcessed(workerId);
 
             console.log(
                 `[${workerId}] completed job ${job.id} attempt=${job.attempts}`
